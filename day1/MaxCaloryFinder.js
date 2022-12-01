e09935175d3d693431a1d34888ccc8105039ef3b
Sobhan Aminnejad <s.aminnejad@catawiki.com>
2022-12-01 19:43:52 +0100

    first day

---
+++ day1/MaxCaloryFinder.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const maxCalories = () => {
+  let sums = [];
+  let backpacker = [];
+  const allFileContents = fs.readFileSync("./input.txt", "utf-8");
+  allFileContents.split(/\r?\n/).forEach((line) => {
+    let calories = Number(line);
+    backpacker.push(calories);
+
+    if (calories === 0) {
+      sums.push(backpacker.reduce((a, b) => a + b, 0));
+      backpacker = [];
+    }
+  });
+  sums.sort((a, b) => b - a);
+  let sumOfThree = sums.slice(0, 3);
+  console.log(sums);
+
+  console.log("Max: ", Math.max(...sums));
+  console.log(
+    "Sum of Three: ",
+    sumOfThree.reduce((a, b) => a + b, 0)
+  );
+};

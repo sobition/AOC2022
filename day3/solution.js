633603945b7619c0158c07b0d5a29fd18a63f683
Sobhan Aminnejad <s.aminnejad@catawiki.com>
2022-12-03 07:22:00 +0100

    day 3

--- day3/solution.js
+++ day3/solution.js
@@ -1,8 +1,8 @@
-function split(str, index) {
+const split = (str, index) => {
   const result = [str.slice(0, index), str.slice(index)];
 
   return result;
-}
+};
 
 const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 (() => {
